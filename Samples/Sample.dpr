e4fa0c1d9d9a6811cc72309cbdfd6f84605d8011
Danilo Lucas <developer.dlio@gmail.com>
2020-10-22 07:10:48 -0300

    Horse-Query

---
+++ Samples/Sample.dpr
@@ -0,0 +1,49 @@
+program Sample;
+
+uses
+  Horse,
+  Horse.Query in '..\src\Horse.Query.pas',
+  FireDAC.Comp.Client, Data.DB,
+  System.SysUtils;
+
+begin
+  THorse.Use(Query);
+
+  THorse.Get('/ping',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    var
+      Memtable: TFDMemtable;
+    begin
+      Memtable := TFDMemtable.Create(nil);
+      Memtable.FieldDefs.Add('Codigo', ftInteger, 0, False);
+      Memtable.FieldDefs.Add('Nome', ftString, 100, False);
+
+      Memtable.LogChanges := False;
+      Memtable.CachedUpdates := True;
+
+      Memtable.Close;
+      Memtable.CreateDataSet;
+      Memtable.Open;
+
+      Memtable.Append;
+      Memtable.FieldByName('Codigo').AsInteger := 1;
+      Memtable.FieldByName('Nome').AsString := 'Ping';
+      Memtable.Post;
+
+      Memtable.Append;
+      Memtable.FieldByName('Codigo').AsInteger := 2;
+      Memtable.FieldByName('Nome').AsString := 'Pong';
+      Memtable.Post;
+
+      Memtable.ApplyUpdates;
+
+      Res.Send<TFDMemtable>(Memtable);
+    end);
+
+  THorse.Listen(9002,
+    procedure(Horse: THorse)
+    begin
+      Writeln(Format('Server started in %s:%d', [THorse.Host, THorse.Port]));
+      Writeln('Press return to stop...');
+    end);
+end.
---
+++ src/Horse.Query.pas
@@ -0,0 +1,48 @@
+unit Horse.Query;
+
+interface
+
+uses
+  Horse,
+  DataSet.Serialize, DataSet.Serialize.Config,
+  Data.DB,
+  System.JSON, System.SysUtils;
+
+procedure Query(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+
+implementation
+
+procedure Query(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var
+  LContent: TObject;
+  LJA: TJSONArray;
+  LOldLowerCamelCase: Boolean;
+begin
+  Next;
+
+  LContent := THorseHackResponse(Res).GetContent;
+
+  if Assigned(LContent) and LContent.InheritsFrom(TDataSet) then
+  begin
+    LOldLowerCamelCase := TDataSetSerializeConfig.GetInstance.LowerCamelCase;
+    TDataSetSerializeConfig.GetInstance.LowerCamelCase := False;
+
+    try
+      LJA := TDataSet(LContent).ToJSONArray;
+    finally
+      TDataSetSerializeConfig.GetInstance.LowerCamelCase := LOldLowerCamelCase;
+    end;
+  end
+  else
+    Exit;
+
+  if Assigned(LJA) then
+  begin
+    Res.Send(LJA.ToString);
+    FreeAndNil(LJA);
+  end
+  else
+    Res.Send('[]');
+end;
+
+end.
